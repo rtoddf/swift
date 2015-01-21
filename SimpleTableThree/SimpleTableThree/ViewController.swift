60eaea7acb2fad811c713aa7b5cef25a4e1179da
Todd Fleeman <rtoddf@comcast.net>
2015-01-21 10:58:13 -0500

    reinforcing tableviewcontrollers

---
+++ SimpleTableThree/SimpleTableThree/ViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class PetTableViewController: UITableViewController {
+    
+    var Pets = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        Pets = ["Harley", "Harper", "Pinky", "Tam", "Tuttle", "Trouble"]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Pets.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = Pets[indexPath.row]
+        
+        return cell
+    }
+    
+}
+
