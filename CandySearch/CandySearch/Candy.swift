149084591fcd28b6a0feb362573af57ea85fcc5b
Todd Fleeman <rtoddf@comcast.net>
2015-01-21 23:09:17 -0500

    next step in tablleviewcontrollers and added candy exmaple

---
+++ CandySearch/CandySearch/Candy.swift
@@ -0,0 +1,13 @@
+//
+//  Candy.swift
+//  CandySearch
+//
+//  Copyright (c) 2014 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+
+struct Candy {
+  let category : String
+  let name : String
+}
