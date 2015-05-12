88e76e7f65f249c8d3f70cde433adadb0e60ad54
Todd Fleeman <rtoddf@comcast.net>
2015-05-12 18:31:58 -0400

    cleaning more things up

--- IntermediateTableFour/IntermediateTableFour/VideoGameDetail.swift
+++ IntermediateTableFour/IntermediateTableFour/VideoGameDetail.swift
@@ -1,6 +1,25 @@
 import Foundation
 import UIKit
 
+
+// per http://www.developerdave.co.uk/2014/10/gradient-backgrounds-swift/
+extension CAGradientLayer {
+    
+    func turquoiseColor() -> CAGradientLayer {
+        let topColor = UIColor(red: (15/255.0), green: (118/255.0), blue: (128/255.0), alpha: 1)
+        let bottomColor = UIColor(red: (84/255.0), green: (187/255.0), blue: (187/255.0), alpha: 1)
+        
+        let gradientColors: Array <AnyObject> = [topColor.CGColor, bottomColor.CGColor]
+        let gradientLocations: Array <AnyObject> = [0.0, 1.0]
+        
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
+        gradientLayer.colors = gradientColors
+        gradientLayer.locations = gradientLocations
+        
+        return gradientLayer
+    }
+}
+
 class VideoGameDetail: UIViewController {
 //    var gameTitleText = String()
 //    var characterText = String()
@@ -12,10 +31,30 @@ class VideoGameDetail: UIViewController {
     override func viewDidLoad() {
         self.view.backgroundColor = UIColor.blackColor()
         
+//        let background = CAGradientLayer().turquoiseColor()
+//        background.frame = self.view.bounds
+//        self.view.layer.insertSublayer(background, atIndex: 0)
+        
+        let myGradientLayer: CAGradientLayer
+        myGradientLayer = CAGradientLayer()
+        myGradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        myGradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        let colors: [CGColorRef] = [
+            UIColor.clearColor().CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+            UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+            UIColor.clearColor().CGColor ]
+        myGradientLayer.colors = colors
+        myGradientLayer.opaque = false
+        myGradientLayer.locations = [0.0,  0.3, 0.5, 0.7, 1.0]
+        self.view.layer.addSublayer(myGradientLayer)
+        
         var imageHeight = view.bounds.size.width * 0.66
         
         var leadImageView = UIImageView(frame: CGRectMake(0, 65, view.bounds.size.width, imageHeight))
         var image = UIImage(named: game!.leadImage)
+        
         leadImageView.image = image
         
         var title = UILabel(frame: CGRectMake(0, 75, view.bounds.size.width-10, 21))
@@ -37,7 +76,9 @@ class VideoGameDetail: UIViewController {
         bodyCopy.numberOfLines = 0
         bodyCopy.lineBreakMode = NSLineBreakMode.ByWordWrapping
         
+        
         self.view.addSubview(leadImageView)
+       
         self.view.addSubview(title)
         self.view.addSubview(mainCharacter)
         self.view.addSubview(bodyCopy)
