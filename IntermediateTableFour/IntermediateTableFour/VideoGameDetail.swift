782445bea806b0554db0639fdb3c02523e2ca229
Todd Fleeman <rtoddf@comcast.net>
2015-05-13 17:57:54 -0400

    added gradient and remainder of data

--- IntermediateTableFour/IntermediateTableFour/VideoGameDetail.swift
+++ IntermediateTableFour/IntermediateTableFour/VideoGameDetail.swift
@@ -3,22 +3,22 @@ import UIKit
 
 
 // per http://www.developerdave.co.uk/2014/10/gradient-backgrounds-swift/
-extension CAGradientLayer {
-    
-    func turquoiseColor() -> CAGradientLayer {
-        let topColor = UIColor(red: (15/255.0), green: (118/255.0), blue: (128/255.0), alpha: 1)
-        let bottomColor = UIColor(red: (84/255.0), green: (187/255.0), blue: (187/255.0), alpha: 1)
-        
-        let gradientColors: Array <AnyObject> = [topColor.CGColor, bottomColor.CGColor]
-        let gradientLocations: Array <AnyObject> = [0.0, 1.0]
-        
-        let gradientLayer: CAGradientLayer = CAGradientLayer()
-        gradientLayer.colors = gradientColors
-        gradientLayer.locations = gradientLocations
-        
-        return gradientLayer
-    }
-}
+//extension CAGradientLayer {
+//    
+//    func turquoiseColor() -> CAGradientLayer {
+//        let topColor = UIColor(red: (15/255.0), green: (118/255.0), blue: (128/255.0), alpha: 1)
+//        let bottomColor = UIColor(red: (84/255.0), green: (187/255.0), blue: (187/255.0), alpha: 1)
+//        
+//        let gradientColors: Array <AnyObject> = [topColor.CGColor, bottomColor.CGColor]
+//        let gradientLocations: Array <AnyObject> = [0.0, 1.0]
+//        
+//        let gradientLayer: CAGradientLayer = CAGradientLayer()
+//        gradientLayer.colors = gradientColors
+//        gradientLayer.locations = gradientLocations
+//        
+//        return gradientLayer
+//    }
+//}
 
 class VideoGameDetail: UIViewController {
 //    var gameTitleText = String()
@@ -35,20 +35,20 @@ class VideoGameDetail: UIViewController {
 //        background.frame = self.view.bounds
 //        self.view.layer.insertSublayer(background, atIndex: 0)
         
-        let myGradientLayer: CAGradientLayer
-        myGradientLayer = CAGradientLayer()
-        myGradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        myGradientLayer.endPoint = CGPoint(x: 1, y: 1)
-        let colors: [CGColorRef] = [
-            UIColor.clearColor().CGColor,
-            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
-            UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).CGColor,
-            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
-            UIColor.clearColor().CGColor ]
-        myGradientLayer.colors = colors
-        myGradientLayer.opaque = false
-        myGradientLayer.locations = [0.0,  0.3, 0.5, 0.7, 1.0]
-        self.view.layer.addSublayer(myGradientLayer)
+//        let myGradientLayer: CAGradientLayer
+//        myGradientLayer = CAGradientLayer()
+//        myGradientLayer.startPoint = CGPoint(x: 0, y: 0)
+//        myGradientLayer.endPoint = CGPoint(x: 1, y: 1)
+//        let colors: [CGColorRef] = [
+//            UIColor.clearColor().CGColor,
+//            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+//            UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).CGColor,
+//            UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor,
+//            UIColor.clearColor().CGColor ]
+//        myGradientLayer.colors = colors
+//        myGradientLayer.opaque = false
+//        myGradientLayer.locations = [0.0,  0.3, 0.5, 0.7, 1.0]
+//        self.view.layer.addSublayer(myGradientLayer)
         
         var imageHeight = view.bounds.size.width * 0.66
         
@@ -76,9 +76,24 @@ class VideoGameDetail: UIViewController {
         bodyCopy.numberOfLines = 0
         bodyCopy.lineBreakMode = NSLineBreakMode.ByWordWrapping
         
-        
         self.view.addSubview(leadImageView)
-       
+
+        var gradientLayerView: UIView = UIView(frame: CGRectMake(0, 65, view.bounds.size.width, imageHeight))
+        var gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = gradientLayerView.bounds
+        
+        gradient.colors = [
+            UIColor(red: 0, green: 0, blue: 0, alpha: 1).CGColor,
+            UIColor.clearColor().CGColor,
+            UIColor.clearColor().CGColor,
+            UIColor(red: 0, green: 0, blue: 0, alpha: 1).CGColor
+        ]
+        
+        gradient.locations = [ 0.0, 0.2, 0.8, 1.0]
+        
+        gradientLayerView.layer.insertSublayer(gradient, atIndex: 5)
+        self.view.layer.insertSublayer(gradientLayerView.layer, atIndex: 5)
+        
         self.view.addSubview(title)
         self.view.addSubview(mainCharacter)
         self.view.addSubview(bodyCopy)
