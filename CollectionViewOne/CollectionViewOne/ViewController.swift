4736e151858d483b95284358f0a990f4f9490ecc
Todd Fleeman <rtoddf@comcast.net>
2015-05-23 12:45:44 -0400

    catching up

---
+++ CollectionViewOne/CollectionViewOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewOne
+//
+//  Created by Todd Fleeman on 2/4/15.
+//  Copyright (c) 2015 Todd Fleeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
