6fac5055107c19a740445ba8f20cdff7f4b23304
Todd Fleeman <Todd.Fleeman@coxinc.com>
2016-10-24 19:09:19 -0400

    trying a quiz

---
+++ quizzes/CivicsQuiz1/CivicsQuiz1/ViewController.swift
@@ -0,0 +1,21 @@
+//  CivicsQuiz1
+
+//https://www.youtube.com/watch?annotation_id=annotation_4161981239&feature=iv&src_vid=nvug6a_jPRs&v=dyxqsfrCaeM
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
