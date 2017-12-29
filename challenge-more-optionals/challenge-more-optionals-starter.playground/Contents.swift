d30567cacc6468e4559072da5cc958159cf4222f
Todd Fleeman <rtoddf@comcast.net>
2017-12-28 21:13:32 -0500

    more optionals challenge

--- challenge-more-optionals/challenge-more-optionals-starter.playground/Contents.swift
+++ challenge-more-optionals/challenge-more-optionals-starter.playground/Contents.swift
@@ -1,32 +1,3 @@
-/**
- * Copyright (c) 2017 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 
 /*:
  ### MORE OPTIONALS
@@ -35,7 +6,12 @@
  */
 
 // TODO: Write solution here
-
+var myFavoriteSong:String? = "Penny Lane"
+if let myFavoriteSong = myFavoriteSong {
+    print("My favorite song is \(myFavoriteSong).")
+} else {
+    print("I don't have a favorite song.")
+}
 /*:
  Change `myFavoriteSong` to the opposite of what it is now. If it’s nil, set it to a string; if it’s a string, set it to nil. Observe how your printed result changes.
  */
@@ -47,3 +23,12 @@
  */
 
 // TODO: Write solution here
+func printNickname(_ knick:String?) {
+    guard let knick = knick else {
+        print("Error")
+        return
+    }
+    print("my knickname is \(knick)")
+}
+
+printNickname("honey man")
