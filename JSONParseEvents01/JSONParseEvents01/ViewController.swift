219dc1dd37d9ebe6a5f105abbe98d91db3f57e09
Todd Fleeman <rtoddf@comcast.net>
2018-01-05 20:41:06 -0500

    working on json parsing

--- JSONParseEvents01/JSONParseEvents01/ViewController.swift
+++ JSONParseEvents01/JSONParseEvents01/ViewController.swift
@@ -2,6 +2,14 @@ import UIKit
 
 enum ImageSize {
     case blackborder250
+    /*
+        some image sizes available
+        blackborder250    (250 x 250)
+        blackborder500    (500 x 500)
+        block250    (250 x 250)
+        block67        (67 x 67)
+        large        (480 x 480)
+    */
 }
 
 struct EventFeedDescription: Decodable {
@@ -29,9 +37,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let page_size:Int = 4
+        let api_key:String = "c8D6Z2pDmt3Pfg7s"
+        let api_token:String = "da55a4a4bd36478372dc17090ed99ac2"
+        let sort_by:String = "date"
+        let query_start_date:String = "2018010310"
+        let query_end_date:String = "2018060210"
+        let page_size:Int = 25
         let image_size:String = "blackborder250"
-        let jsonUrlString = getApiEndpoint(forNumberOfResults: page_size, withImageSize: image_size)
+        let site_url:String = "http%3A%2F%2Fevents.k923orlando.com"
+        
+        
+        
+        let jsonUrlString = getApiEndpoint(forApiKey: api_key, forApiToken: api_token, sortBy:sort_by, startDate: query_start_date, endDate: query_end_date, forNumberOfResults: page_size, withImageSize: image_size, withSiteUrl: site_url)
         guard let url = URL(string: jsonUrlString) else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, err) in
@@ -40,13 +57,12 @@ class ViewController: UIViewController {
             
             do {
                 let feedDescription  = try JSONDecoder().decode(EventFeedDescription.self, from: data)
-                print("User: ", feedDescription.api_results.user ?? "")
-                print("Page size: ", feedDescription.api_results.page_size ?? "")
+//                print("User: ", feedDescription.api_results.user ?? "")
+//                print("Page size: ", feedDescription.api_results.page_size ?? "")
                 
                 for event in feedDescription.api_results.events {
                     print("Event title: ", event.title ?? "")
                     print("@", event.venue_name ?? "")
-                    print("Time String", event.time_string ?? "")
                     
                     // can this be moved to the struct??
                     let startTime = event.start_time
@@ -59,6 +75,7 @@ class ViewController: UIViewController {
                     print("Start Time", startTimeFormatted ?? "")
                     print("Stop Time", stopTimeFormatted ?? "")
                     print("Event image: ", eventImage ?? "")
+                    print("-------------------------------")
                 }
                 
             } catch let jsonErr {
@@ -71,9 +88,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
 
-    func getApiEndpoint(forNumberOfResults pageSize:Int, withImageSize imageSize:String) -> String {
-        let endpoint:String = "https://staging.k923orlando.com/api/eventful-no-callback?callback=EventCallback&include=tickets&show_tickets=1&app_key=c8D6Z2pDmt3Pfg7s&api_token=da55a4a4bd36478372dc17090ed99ac2&q=K92.3%2BBest%2BBets&sort_by=date&w=2018010310-2018020210&page_size=" + String(pageSize) + "&image_size=\(imageSize)&ehp_tag=&SiteUrl=http%3A%2F%2Fevents.k923orlando.com&ApiUrl=%2Fapi%2Fv1%2Fevents%2F&ApiMethod=search"
+    func getApiEndpoint(forApiKey apiKey:String, forApiToken apiToken:String, sortBy sort:String, startDate forStartDate:String, endDate toEndDate:String, forNumberOfResults pageSize:Int, withImageSize imageSize:String, withSiteUrl siteUrl:String) -> String {
+        let endpoint:String = "https://staging.k923orlando.com/api/eventful-no-callback?callback=EventCallback&include=tickets&show_tickets=1&app_key=\(apiKey)&api_token=\(apiToken)&q=K92.3%2BBest%2BBets&sort_by=\(sort)&w=\(forStartDate)-\(toEndDate)&page_size=" + String(pageSize) + "&image_size=\(imageSize)&ehp_tag=&SiteUrl=\(siteUrl)&ApiUrl=%2Fapi%2Fv1%2Fevents%2F&ApiMethod=search"
         return endpoint
+        
+        // https://staging.k923orlando.com/api/eventful-no-callback?callback=EventCallback&include=tickets&show_tickets=1&app_key=c8D6Z2pDmt3Pfg7s&api_token=da55a4a4bd36478372dc17090ed99ac2&q=K92.3%2BBest%2BBets&sort_by=date&w=2018010310-2018060210&page_size=25&image_size=blackborder250&ehp_tag=&SiteUrl=http%3A%2F%2Fevents.k923orlando.com&ApiUrl=%2Fapi%2Fv1%2Fevents%2F&ApiMethod=search
     }
 }
 
--- TableView01-DeanAllen/TableView01-DeanAllen/VideoListScreen.swift
+++ TableView01-DeanAllen/TableView01-DeanAllen/VideoListScreen.swift
@@ -1,11 +1,12 @@
 import UIKit
 
+// https://www.youtube.com/watch?v=FtO5QT2D_H8&list=PL8seg1JPkqgFQQI7skvLoDeSv0vQ_IM5Q
+// https://www.youtube.com/watch?v=gN3FbNJ6_TY&list=PL8seg1JPkqgG3822Pbf_H9xldrODNV3XI&index=9 - pass data
 // this is the controller for the tableview
 class VideoListScreen: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     var videos:[Video] = []
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         videos = createArray()
@@ -24,6 +25,9 @@ class VideoListScreen: UIViewController {
         let video4 = Video(image: #imageLiteral(resourceName: "colton-haynes"), title: "Colton Haynes")
         let video5 = Video(image: #imageLiteral(resourceName: "eli-lieb"), title: "Eli Lieb")
         let video6 = Video(image: #imageLiteral(resourceName: "gus-kenworthy"), title: "Gus Kenworthy")
+        let video7 = Video(image: #imageLiteral(resourceName: "matt-doyle"), title: "Matt Doyle")
+        let video8 = Video(image: #imageLiteral(resourceName: "shaping-sound"), title: "Shaping Sound")
+        let video9 = Video(image: #imageLiteral(resourceName: "tom-daley"), title: "Tom Daley")
         
         tempVideos.append(video1)
         tempVideos.append(video2)
@@ -31,6 +35,9 @@ class VideoListScreen: UIViewController {
         tempVideos.append(video4)
         tempVideos.append(video5)
         tempVideos.append(video6)
+        tempVideos.append(video7)
+        tempVideos.append(video8)
+        tempVideos.append(video9)
         
         return tempVideos
     }
@@ -88,6 +95,7 @@ class VideoListScreen: UIViewController {
 
 }
 
+// extension for add datasource and delegate protocols to the tableview 
 extension VideoListScreen: UITableViewDataSource, UITableViewDelegate {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
