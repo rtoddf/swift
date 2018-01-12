11e73b26ed59e9e17c27d9000a0699956214c4b3
Tod Fleeman <rtoddf@me.com>
2018-01-11 19:13:33 -0500

    consolidating download images

--- ImageDownload01 - Percival/ImageDownload01 - Percival/ViewController.swift
+++ ImageDownload01 - Percival/ImageDownload01 - Percival/ViewController.swift
@@ -5,12 +5,79 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let imageUrl:String = "https://www.whio.com/rf/image_lowres/Pub/p9/JournalNews/2018/01/11/Images/newsEngin.20935433_Jocelyn-Bucaro.jpg"
+        
+        getWebImage(imageUrl)
+        
+//        swift 4 return a uiimage from function
+//        https://stackoverflow.com/questions/31608302/return-value-from-completion-handler-swift/31608684
+        
+//        var theimage:UIImage = getWebImage(imageUrl)
+        
+        // restores from memory if downloaded
+//        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+//        if documentsPath.count > 0 {
+//            let documentsDirectory = documentsPath[0]
+//
+//            if let savedImageName = savedImageName {
+//                let restorePath = documentsDirectory + savedImageName
+//                imageView.image = UIImage(contentsOfFile: restorePath)
+//            }
+//        }
+        
+        // downloads the image the first time
+//        let url = URL(string: imageUrl)!
+//        let request = NSMutableURLRequest(url: url)
+//        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+//            data, response, error in
+//
+//            if error != nil {
+//                print(error as Any)
+//            } else {
+//                if let data = data {
+//                    if let image = UIImage(data: data) {
+//                        DispatchQueue.main.async { // Correct - https://developer.apple.com/documentation/code_diagnostics/main_thread_checker
+//                            self.imageView.image = image
+//
+//                            // searches for documents path
+//                            let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+//                            // should return one, but we just make sure
+//                            if documentsPath.count > 0 {
+//                                if let savedImageName = savedImageName {
+//                                    let documentsDirectory = documentsPath[0]
+//                                    let savePath = documentsDirectory + savedImageName
+//
+//                                    do {
+//                                        try UIImageJPEGRepresentation(image, 1)?.write(to: URL(fileURLWithPath: savePath))
+//                                    } catch {
+//                                        // process the error
+//                                    }
+//
+//                                    // another way
+//                                    // FileManager.default.createFile(atPath: savePath, contents: data, attributes: nil)
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func getWebImage(_ imageUrl:String) {
         let imageNameSplit = imageUrl.components(separatedBy: "/")
         let savedImageName = imageNameSplit.last
         
-        // restores from memory if downloaded
+        let url = URL(string: imageUrl)!
+        let request = NSMutableURLRequest(url: url)
+        
         let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         if documentsPath.count > 0 {
             let documentsDirectory = documentsPath[0]
@@ -21,9 +88,6 @@ class ViewController: UIViewController {
             }
         }
         
-        // downloads the image the first time
-        let url = URL(string: imageUrl)!
-        let request = NSMutableURLRequest(url: url)
         let task = URLSession.shared.dataTask(with: request as URLRequest) {
             data, response, error in
             
@@ -34,6 +98,10 @@ class ViewController: UIViewController {
                     if let image = UIImage(data: data) {
                         DispatchQueue.main.async { // Correct - https://developer.apple.com/documentation/code_diagnostics/main_thread_checker
                             self.imageView.image = image
+                            
+                            print(type(of: image))
+                            
+//                            return image
                             
                             // searches for documents path
                             let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
@@ -60,10 +128,6 @@ class ViewController: UIViewController {
         }
         
         task.resume()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
     }
 
 
