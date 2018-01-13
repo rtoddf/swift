02d5efec777277b6188d81ef36f6d607f14fda61
Todd Fleeman <rtoddf@comcast.net>
2018-01-12 23:46:18 -0500

    addedplay button if video

--- WHIOTableView01/WHIOTableView01/ArticleCell.swift
+++ WHIOTableView01/WHIOTableView01/ArticleCell.swift
@@ -4,9 +4,11 @@ class ArticleCell: UITableViewCell {
 
     @IBOutlet weak var articleThumbnailImage: UIImageView!
     @IBOutlet weak var articleTitleLabel: UILabel!
+    @IBOutlet weak var playButton: UIImageView!
     
     func setArticle(article: Item) {
         articleTitleLabel.text = article.title
+        
         
         var imageUrl:String = ""
         
@@ -14,6 +16,7 @@ class ArticleCell: UITableViewCell {
             imageUrl = article.images![0].url!
         } else if article.videos != nil {
             imageUrl = article.videos![0].image.url!
+            playButton.image = #imageLiteral(resourceName: "playbutton")
         } else {
             imageUrl = "https://www.whio.com/assets/images/whio-placeholder.png"
         }
