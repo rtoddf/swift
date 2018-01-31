444ce39841c9e5d36a93c70ebea2ce90d831a61d
Tod Fleeman <rtoddf@me.com>
2018-01-31 12:57:01 -0500

    cleaning up code - part 1

---
+++ AutoEasyLayoutExtensions/AutoEasyLayoutExtensions/Extensions/Helpers.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+extension UIColor {
+    convenience init(hexString:String, alpha:CGFloat = 1.0) {
+        let hexString:String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+        
+        if hexString.hasPrefix("#") {
+            scanner.scanLocation = 1
+        }
+        
+        var color:UInt32 = 0
+        scanner.scanHexInt32(&color)
+        
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+        
+        let red = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue = CGFloat(b) / 255.0
+        
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1.0)
+    }
+}
+
+extension UIView {
+    func anchor(top:NSLayoutYAxisAnchor?, leading:NSLayoutXAxisAnchor?, bottom:NSLayoutYAxisAnchor?, trailing:NSLayoutXAxisAnchor?, padding:UIEdgeInsets = .zero, size:CGSize = .zero) {
+        
+        let viewCornerRadius:CGFloat = 8.0
+        let viewBackgroundColor = UIColor(hexString: "#333333")
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = viewBackgroundColor
+        layer.cornerRadius = viewCornerRadius
+        clipsToBounds = true
+        
+        //        topAnchor.constraint(equalTo: <#T##NSLayoutAnchor<NSLayoutYAxisAnchor>#>, constant: <#T##CGFloat#>)
+        
+        //        guard let top = top else { return }
+        //        guard let leading = leading else { return }
+        //        guard let bottom = bottom else { return }
+        //        guard let trailing = trailing else { return }
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+        // bottom and right padding have to be nagative
+    }
+}
---
+++ AutoEasyLayoutExtensions/AutoEasyLayoutExtensions/Views/ViewController.swift
@@ -0,0 +1,64 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpViews()
+    }
+    
+    func setUpViews() {
+        let viewPadding:CGFloat = 12.0
+        
+        let smallViewWidth = (view.frame.width * 0.333) - viewPadding
+        let largeViewWidth = (view.frame.width * 0.67) - viewPadding
+        
+        let firstImageView = UIImageView()
+        firstImageView.image = UIImage(named: "adam-lambert")
+        firstImageView.frame = CGRect(x: 0, y: 0, width: smallViewWidth, height: smallViewWidth)
+        firstImageView.contentMode = .scaleAspectFill
+        
+        let firstView = UIView()
+        firstView.addSubview(firstImageView)
+        
+        let secondImageView = UIImageView()
+        secondImageView.image = UIImage(named: "ben-barnes")
+        secondImageView.frame = CGRect(x: 0, y: 0, width: smallViewWidth, height: smallViewWidth)
+        secondImageView.contentMode = .scaleAspectFill
+        
+        let secondView = UIView()
+        secondView.addSubview(secondImageView)
+        
+        let thirdImageView = UIImageView()
+        thirdImageView.image = UIImage(named: "jonathan-groff")
+        thirdImageView.frame = CGRect(x: 0, y: 0, width: largeViewWidth, height: largeViewWidth)
+        thirdImageView.contentMode = .scaleAspectFit
+        
+        let thirdView = UIView()
+        thirdView.addSubview(thirdImageView)
+        
+        [firstView, secondView, thirdView].forEach { view.addSubview($0) }
+        
+        
+        
+        //        view.addSubview(firstView)
+        //        view.addSubview(secondView)
+        
+        // enables autolayout
+        //        firstView = false
+        //
+        //        firstView(equalTo: view.topAnchor).isActive = true
+        //        firstView(equalTo: view.leadingAnchor).isActive = true
+        //        firstView(equalTo: view.trailingAnchor).isActive = true
+        //        firstView(equalTo: view.bottomAnchor).isActive = true
+        
+        //        firstView(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
+        
+        firstView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: CGFloat(viewPadding)), size: .init(width: smallViewWidth, height: smallViewWidth))
+        
+        secondView.anchor(top: firstView.bottomAnchor, leading: nil, bottom: nil, trailing: firstView.trailingAnchor, padding: .init(top: viewPadding, left: 0, bottom: 0, right: 0) , size: .init(width: smallViewWidth, height: smallViewWidth))
+        
+        thirdView.anchor(top: firstView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: secondView.bottomAnchor, trailing: firstView.leadingAnchor, padding: .init(top: 0, left: viewPadding, bottom: 0, right: viewPadding))
+    }
+
+}
