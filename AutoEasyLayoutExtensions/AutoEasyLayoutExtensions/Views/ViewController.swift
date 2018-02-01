4752fe0979651d8f045e5e122c1b5abd94b5fe4e
Tod Fleeman <rtoddf@me.com>
2018-02-01 16:15:05 -0500

    moving view around to be in numerical order

--- AutoEasyLayoutExtensions/AutoEasyLayoutExtensions/Views/ViewController.swift
+++ AutoEasyLayoutExtensions/AutoEasyLayoutExtensions/Views/ViewController.swift
@@ -14,19 +14,22 @@ class ViewController: UIViewController {
         let largeViewWidth = (view.frame.width * 0.67)
         
         let firstImageView = UIImageView()
-        firstImageView.image = UIImage(named: "adam-lambert")
-        firstImageView.frame = CGRect(x: 0, y: 0, width: smallViewWidth, height: smallViewWidth)
-        firstImageView.contentMode = .scaleAspectFill
-
+        firstImageView.image = UIImage(named: "jonathan-groff")
+        firstImageView.frame = CGRect(x: 0, y: 0, width: largeViewWidth, height: largeViewWidth)
+        firstImageView.contentMode = .scaleAspectFit
+        
+        
         let secondImageView = UIImageView()
-        secondImageView.image = UIImage(named: "ben-barnes")
+        secondImageView.image = UIImage(named: "adam-lambert")
         secondImageView.frame = CGRect(x: 0, y: 0, width: smallViewWidth, height: smallViewWidth)
         secondImageView.contentMode = .scaleAspectFill
 
         let thirdImageView = UIImageView()
-        thirdImageView.image = UIImage(named: "adam-lambert")
-        thirdImageView.frame = CGRect(x: 0, y: 0, width: largeViewWidth, height: largeViewWidth)
-        thirdImageView.contentMode = .scaleAspectFit
+        thirdImageView.image = UIImage(named: "ben-barnes")
+        thirdImageView.frame = CGRect(x: 0, y: 0, width: smallViewWidth, height: smallViewWidth)
+        thirdImageView.contentMode = .scaleAspectFill
+
+        
         
         
         
@@ -58,17 +61,19 @@ class ViewController: UIViewController {
         
         //        firstView(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 16, bottom: 0, right: 16))
         
-        firstView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: smallViewWidth, height: smallViewWidth))
+        firstView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: nil, trailing: nil, size: .init(width: largeViewWidth, height: largeViewWidth))
         
-        secondView.anchor(top: firstView.bottomAnchor, leading: nil, bottom: nil, trailing: firstView.trailingAnchor, size: .init(width: smallViewWidth, height: smallViewWidth))
+        secondView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: firstView.trailingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: smallViewWidth, height: smallViewWidth))
         
-        thirdView.anchor(top: firstView.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: secondView.bottomAnchor, trailing: firstView.leadingAnchor)
+        thirdView.anchor(top: secondView.bottomAnchor, leading: secondView.leadingAnchor, bottom: firstView.bottomAnchor, trailing: secondView.trailingAnchor)
         
-        fourthView.anchor(top: thirdView.bottomAnchor, leading: thirdView.leadingAnchor, bottom: nil, trailing: nil, size: .init(width: smallViewWidth, height: smallViewWidth))
         
-        fifthView.anchor(top: thirdView.bottomAnchor, leading: fourthView.trailingAnchor, bottom: nil, trailing: nil, size: .init(width: smallViewWidth, height: smallViewWidth))
         
-        sixthView.anchor(top: thirdView.bottomAnchor, leading: fifthView.trailingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: smallViewWidth, height: smallViewWidth))
+        fourthView.anchor(top: firstView.bottomAnchor, leading: firstView.leadingAnchor, bottom: nil, trailing: nil, size: .init(width: smallViewWidth, height: smallViewWidth))
+        
+        fifthView.anchor(top: firstView.bottomAnchor, leading: fourthView.trailingAnchor, bottom: nil, trailing: nil, size: .init(width: smallViewWidth, height: smallViewWidth))
+        
+        sixthView.anchor(top: firstView.bottomAnchor, leading: fifthView.trailingAnchor, bottom: nil, trailing: view.safeAreaLayoutGuide.trailingAnchor, size: .init(width: smallViewWidth, height: smallViewWidth))
     }
 
 }
