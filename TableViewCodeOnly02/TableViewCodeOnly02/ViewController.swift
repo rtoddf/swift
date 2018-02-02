c010f7ea88034ea3930ff99af0b498e572ebf11b
Todd Fleeman <rtoddf@comcast.net>
2018-02-01 23:14:16 -0500

    started tableview code only two

---
+++ TableViewCodeOnly02/TableViewCodeOnly02/ViewController.swift
@@ -0,0 +1,26 @@
+/*
+    Swift: Create a Questionnaire App using UITableView Programmatically (Part 1)
+    https://www.youtube.com/watch?v=yl2m4fDOLQo
+ 
+    Swift: Create a Questionnaire App using UITableView Programmatically (Part 2)
+    https://www.youtube.com/watch?v=iLIieGckVKs
+*/
+
+import UIKit
+
+class QuestionController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Question"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
