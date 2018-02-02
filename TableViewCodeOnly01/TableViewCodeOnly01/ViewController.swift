6698ddd0391a0ccad358a34a0cdfe368191b69df
Todd Fleeman <rtoddf@comcast.net>
2018-02-01 22:11:28 -0500

    finished the tableview code only

--- TableViewCodeOnly01/TableViewCodeOnly01/ViewController.swift
+++ TableViewCodeOnly01/TableViewCodeOnly01/ViewController.swift
@@ -3,29 +3,47 @@
 import UIKit
 
 class MyTableViewController: UITableViewController {
+    
+    var items = ["item 1", "item 2", "item 3", "item 4"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "My TableView"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Insert", style: .plain, target: self, action: #selector(insert))
+        
         tableView.register(MyCell.self, forCellReuseIdentifier: "cellId")
         tableView.register(Header.self, forHeaderFooterViewReuseIdentifier: "headerId")
         tableView.sectionFooterHeight = 50
+    }
     
-        // Do any additional setup after loading the view, typically from a nib.
+    @objc func insert(){
+        items.append("Item \(items.count + 1)")
+        let insertionIndexPath = NSIndexPath(row: items.count - 1, section: 0)
+        tableView.insertRows(at: [insertionIndexPath as IndexPath], with: .automatic)
+        tableView.reloadData()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        return tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! MyCell
+        myCell.nameLabel.text = items[indexPath.row]
+        myCell.myTableViewController = self
+        return myCell
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return tableView.dequeueReusableHeaderFooterView(withIdentifier: "headerId")
+    }
+    
+    func deleteCell(cell:UITableViewCell){
+        if let deletionIndexPath = tableView.indexPath(for: cell){
+            items.remove(at: deletionIndexPath.row)
+            tableView.deleteRows(at: [deletionIndexPath], with: .automatic)
+        }
     }
 }
 
@@ -56,6 +74,8 @@ class Header:UITableViewHeaderFooterView {
 }
 
 class MyCell:UITableViewCell{
+    var myTableViewController:MyTableViewController?
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupViews()
@@ -73,9 +93,31 @@ class MyCell:UITableViewCell{
         return label
     }()
     
+    let actionButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Delete", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     func setupViews() {
         addSubview(nameLabel)
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addSubview(actionButton)
+        
+        actionButton.addTarget(self, action: #selector(handleAction), for: .touchUpInside)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-8-[v1(80)]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": actionButton]))
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": actionButton ]))
+    }
+    
+    @objc func handleAction(){
+        myTableViewController?.deleteCell(cell: self)
     }
 }
+
+
+
+
+
+
