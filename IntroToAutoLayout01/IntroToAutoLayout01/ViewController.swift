8d2170f41e8c90c3fe60a3b08660dcd29f350135
Todd Fleeman <rtoddf@comcast.net>
2018-02-12 00:13:47 -0500

    finished ep 3

--- IntroToAutoLayout01/IntroToAutoLayout01/ViewController.swift
+++ IntroToAutoLayout01/IntroToAutoLayout01/ViewController.swift
@@ -36,17 +36,60 @@ class ViewController: UIViewController {
         return tv
     }()
     
+    // look up encapsulation - private and fileprivate
+    private let previousButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Prev", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Black", size: 14.0)
+        button.setTitleColor(.gray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let nextButton:UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir-Black", size: 14.0)
+        button.setTitleColor(.gray, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let pageControl:UIPageControl = {
+        let pageCont = UIPageControl()
+        pageCont.currentPage = 0
+        pageCont.numberOfPages = 4
+        pageCont.currentPageIndicatorTintColor = UIColor(hexString: "#ae0000")
+        pageCont.pageIndicatorTintColor = UIColor.gray
+        return pageCont
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(topHalfView)
-        topHalfView.addSubview(imageView)
-        view.addSubview(textView)
+
         setLayout()
+        setupBottomControls()
 //        printFonts()
     }
     
+    func setupBottomControls() {
+        let bottomControlsStackView = UIStackView(arrangedSubviews: [previousButton, pageControl, nextButton])
+        bottomControlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomControlsStackView.distribution = .fillEqually
+        view.addSubview(bottomControlsStackView)
+        
+        bottomControlsStackView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        bottomControlsStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        bottomControlsStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        bottomControlsStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
     func setLayout() {
+        view.addSubview(topHalfView)
+        topHalfView.addSubview(imageView)
+        view.addSubview(textView)
+        
         topHalfView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
         topHalfView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 1).isActive = true
         topHalfView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 1).isActive = true
@@ -55,7 +98,6 @@ class ViewController: UIViewController {
         
         imageView.centerXAnchor.constraint(equalTo: topHalfView.centerXAnchor).isActive = true
         imageView.centerYAnchor.constraint(equalTo: topHalfView.centerYAnchor).isActive = true
-//        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         imageView.widthAnchor.constraint(equalTo: topHalfView.widthAnchor, multiplier: 1).isActive = true
         imageView.heightAnchor.constraint(equalTo: topHalfView.heightAnchor, multiplier: 1).isActive = true
         
