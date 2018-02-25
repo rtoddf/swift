8c45547942b96dce06c1ed68ae086254dd92e48b
Todd Fleeman <rtoddf@comcast.net>
2018-02-24 21:53:35 -0500

    trying something

--- Parsing101a/Parsing101a/ViewController.swift
+++ Parsing101a/Parsing101a/ViewController.swift
@@ -18,40 +18,52 @@ var people = [Person]()
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let urlString = "http://rtodd.net/swift/data/apps05.json"
-        guard let url = URL(string: urlString) else { return }
         
-        URLSession.shared.dataTask(with: url) { (data, response, err) in
-            // check error
-            
-            guard let data = data else { return }
-//            let dataAsString = String(data: data, encoding: .utf8)
-            
-            do {
-//                let urlString = "http://rtodd.net/swift/data/apps03.json"
-//                let person = try JSONDecoder().decode(Person.self, from: data)
-//                print(person)
-
-//                let urlString = "http://rtodd.net/swift/data/apps04.json"
-//                let people = try JSONDecoder().decode([Person].self, from: data)
-//                print(people)
-                
-//                let urlString = "http://rtodd.net/swift/data/apps05.json"
-                let feed = try JSONDecoder().decode(Feed.self, from: data)
-                
-                guard let peeps = feed.people else { return }
-                
-                people = peeps
-                
-                print(people)
-                
-            } catch let jsonErr {
-                print(jsonErr)
-            }
-            
-        }.resume()
+        var completionHandler: ([Person]) -> Void
         
+        parseJSON(completionHandler: {result in
+            print(result.count)
+        })
+        
+        print("something: \(something)")
     }
+}
+
+func parseJSON(completionHandler: @escaping (([Person]) -> Void)){
+    let urlString = "http://rtodd.net/swift/data/apps05.json"
+    guard let url = URL(string: urlString) else { return }
+    
+    URLSession.shared.dataTask(with: url) { (data, response, err) in
+        // check error
+        
+        guard let data = data else { return }
+        //            let dataAsString = String(data: data, encoding: .utf8)
+        
+        do {
+            //                let urlString = "http://rtodd.net/swift/data/apps03.json"
+            //                let person = try JSONDecoder().decode(Person.self, from: data)
+            //                print(person)
+            
+            //                let urlString = "http://rtodd.net/swift/data/apps04.json"
+            //                let people = try JSONDecoder().decode([Person].self, from: data)
+            //                print(people)
+            
+            //                let urlString = "http://rtodd.net/swift/data/apps05.json"
+            let feed = try JSONDecoder().decode(Feed.self, from: data)
+            
+            guard let peeps = feed.people else { return }
+            
+            completionHandler(peeps)
+        } catch let jsonErr {
+            print(jsonErr)
+        }
+
+        print("done")
+        
+        //            DispatchQueue.main.async {
+        //                self.view.reloadData()
+        //            }
+        
+        }.resume()
 }
 
