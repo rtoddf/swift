a8e143dcf53fd701a9a66a205faee1a4b5694253
Todd Fleeman <Todd.Fleeman@coxinc.com>
2018-04-23 16:47:37 -0400

    added custom cell

--- TableViewNoStoryboard01/TableViewNoStoryboard01/Views/ViewController.swift
+++ TableViewNoStoryboard01/TableViewNoStoryboard01/Views/ViewController.swift
@@ -10,12 +10,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CustomCell
         let text = tableData[indexPath.row]
-        cell.textLabel?.text = text
+        cell.nameLabel.text = text
         return cell
     }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +25,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let tableView:UITableView = {
             let tv = UITableView(frame: self.view.bounds, style: UITableViewStyle.plain)
             tv.backgroundColor = .white
-            tv.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+            tv.register(CustomCell.self, forCellReuseIdentifier: cellId)
             return tv
         }()
 
@@ -34,6 +33,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
 
         view.addSubview(tableView)
+    }
+}
+
+class CustomCell:UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupViews()
+    }
+    
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Label text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        return label
+    }()
+    
+    let descriptionLabel:UILabel = {
+        let label = UILabel()
+        label.text = "Label text"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews(){
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": descriptionLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]-4-[v1]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": nameLabel, "v1": descriptionLabel]))
     }
 }
 
